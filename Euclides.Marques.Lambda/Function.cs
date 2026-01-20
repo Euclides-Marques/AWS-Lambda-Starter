f1decd5ab684ecfcd6bef5009be8c6405a5f1b82
Euclides Marques <euclidesmarques270@gmail.com>
2026-01-20 19:14:05 -0300

    feat: Criação de nova classe de usuário para por em letra maiúscula o nome do usuário

--- Euclides.Marques.Lambda/Function.cs
+++ Euclides.Marques.Lambda/Function.cs
@@ -14,8 +14,14 @@ public class Function
     /// <param name="input">The event for the Lambda function handler to process.</param>
     /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
     /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public string FunctionHandler(User input, ILambdaContext context)
     {
-        return input.ToUpper();
+        return input.Name.ToUpper();
     }
+}
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
 }
